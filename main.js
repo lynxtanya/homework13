74a2c29c95ced69065fc5b4909069e521150b6fa
lynxtanya <122545096+lynxtanya@users.noreply.github.com>
2023-01-24 14:18:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,14 @@
 // Число, яке треба піднести до ступеню, передається як перший аргумент у функції;
 // Ступінь передається як другий аргумент у функціюpow (num, degree).
 
-let num = 1;
-let degree = 1;
-let powNew = 1;
-
 function pow(num, degree) {
-    powNew = num ** degree;
-    console.log(num + ' в ступені ' + degree + ' буде ' + powNew);
-}
-
-pow (2, 3);
+    if (degree == 1) {
+      return num;
+    } else {
+      return num * pow(num, degree - 1);
+    }
+  }
+  
+pow(2, 3);
 
 
